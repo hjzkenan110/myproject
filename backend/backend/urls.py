23e899180be1d64b59b48e1f57eed4071737ba6e
hjz <841641127@qq.com>
2018-08-07 13:57:49 +0800

    第一版，完成

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from query import views as query  # new
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('timelion', query.query_timelion),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='ajax.html')),
 ]
