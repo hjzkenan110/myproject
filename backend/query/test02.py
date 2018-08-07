3f64d8a76e8ef84fe7208279c7a7b57c5da8bf6b
hjz <841641127@qq.com>
2018-08-07 18:19:44 +0800

    test02写好了查询

--- backend/query/test02.py
+++ backend/query/test02.py
@@ -41,4 +41,8 @@ engine = create_engine('mysql+pymysql://itoffice:itoffice@192.168.127.129:3306/h
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 # result = session.query(timelion.fid, func.sum(timelion.unum)).filter(timelion.updatetime<now_time, timelion.updatetime>yes_time).group_by(timelion.fid).order_by(desc(func.sum(timelion.unum))).limit(50)	
+result = session.query(timelion.fid, timelion.updatetime, func.extract('day', timelion.updatetime).label('day'), func.extract('hour', timelion.updatetime).label('h'), func.sum(timelion.unum)).filter(timelion.fid == "34").group_by('h', 'day').limit(50)
+for r in result:
+    pass
+pass
 # pass
